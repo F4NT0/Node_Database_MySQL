a8999b41ed30ecc422169a198e4b01bd188a7098
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-15 21:55:36 -0300

    #17 Create Table Added

--- Source/SQL/basic_commands.sql
+++ Source/SQL/basic_commands.sql
@@ -2,10 +2,21 @@
 -- CREATING A NEW DATABASE ON SQL
 ----------------------------------
 
-create database databaseName
+create database databaseName;
 
 ---------------------------------
 -- REMOVING A NEW DATABASE ON SQL
 ---------------------------------
 
-drop database databaseName
+drop database databaseName;
+
+--------------------------
+-- CREATING A TABLE ON SQL
+--
+-- TYPES:
+-- varchar = String
+-- int = Integer
+-- Date = date 
+--------------------------
+
+create table tableName (var1 varchar(255), var2 int(255));
