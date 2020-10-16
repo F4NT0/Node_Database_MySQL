b3a6aa04a06dc9cd55c01a0aa50f4a5d5812c1ca
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-16 00:32:15 -0300

    CI Configuration

---
+++ Source/CI.js
@@ -0,0 +1,11 @@
+var colors = require('cli-color');
+
+console.log(colors.green("Hello World!"));
+console.log(colors.blue("Hello World!"));
+console.log(colors.red("Hello World!"));
+console.log(colors.yellow("Hello World!"));
+console.log(colors.black("Hello World!"));
+console.log(colors.white("Hello World!"));
+console.log(colors.purple("Hello World!"));
+console.log(colors.magenta("Hello World!"));
+console.log(colors.orange("Hello World!"));
